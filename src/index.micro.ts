6d658701da926b4170c5125795eccd948bdd1550
Benji Stephenson <bjames.stephenson@gmail.com>
2022-03-19 16:22:48 +1300

    basic functions over UTC dates

---
+++ src/index.micro.ts
@@ -0,0 +1,107 @@
+import { DateTime, DateTimeStr } from '.'
+import { assertThat } from 'mismatched'
+import * as fc from 'fast-check'
+
+describe('DateTime', function () {
+  it('gets current date', () => {
+    const now = new Date()
+    const dateTime = DateTime.fromDate(now)
+  })
+
+  it('makes a date from seconds epoch zero', function () {
+    const fromEpoch = DateTime.fromEpoch(0)
+    assertThat(fromEpoch).is({ _tag: 'DateTime', value: '1970-01-01T00:00:00.000+00:00' as DateTimeStr, tz: 'UTC' })
+  })
+
+  it('makes a date from seconds epoch', function () {
+    fc.assert(
+      fc.property(fc.date(), now => {
+        const epochNow = now.valueOf()
+        const expected = DateTime.fromDate(now)
+        const fromEpoch = DateTime.fromEpoch(epochNow)
+
+        assertThat(fromEpoch).is(expected)
+      })
+    )
+  })
+
+  it('converts to a JS Date', function () {
+    fc.assert(
+      fc.property(fc.date(), jsDate => {
+        const dateTime = DateTime.fromDate(jsDate)
+        DateTime.assertIsDateTime(dateTime)
+
+        const toJsDate = DateTime.toDate(dateTime)
+        assertThat(toJsDate).is(jsDate)
+      })
+    )
+  })
+
+  it('isBefore', function () {
+    fc.assert(
+      fc.property(
+        fc.date({ min: DateTime.toDate(DateTime.min), max: DateTime.toDate(DateTime.max) }),
+        fc.date({ min: DateTime.toDate(DateTime.min), max: DateTime.toDate(DateTime.max) }),
+        (x, y) => {
+          const date1 = DateTime.fromDate(x)
+          const date2 = DateTime.fromDate(y)
+
+          DateTime.assertIsDateTime(date1)
+          DateTime.assertIsDateTime(date2)
+
+          const assertion = assertThat(DateTime.isBefore(date1)(date2))
+
+          if (x > y) assertion.withMessage(`${date2} is not before ${date1}`).is(true)
+          else assertion.withMessage(`${date2} is before ${date1}`).is(false)
+        }
+      )
+    )
+  })
+
+  it('isAfter', function () {
+    fc.assert(
+      fc.property(
+        fc.date({ min: DateTime.toDate(DateTime.min), max: DateTime.toDate(DateTime.max) }),
+        fc.date({ min: DateTime.toDate(DateTime.min), max: DateTime.toDate(DateTime.max) }),
+        (x, y) => {
+          const date1 = DateTime.fromDate(x)
+          const date2 = DateTime.fromDate(y)
+
+          DateTime.assertIsDateTime(date1)
+          DateTime.assertIsDateTime(date2)
+
+          const assertion = assertThat(DateTime.isAfter(date1)(date2))
+
+          if (x < y) assertion.withMessage(`${date2} is not after ${date1}`).is(true)
+          else assertion.withMessage(`${date2} is after ${date1}`).is(false)
+        }
+      )
+    )
+  })
+
+  // it('isBetween', function () {
+  //   fc.assert(
+  //     fc.property(
+  //       fc.date({ min: DateTime.toDate(DateTime.min), max: DateTime.toDate(DateTime.max) }),
+  //       fc.date({ min: DateTime.toDate(DateTime.min), max: DateTime.toDate(DateTime.max) }),
+  //       fc.date({ min: DateTime.toDate(DateTime.min), max: DateTime.toDate(DateTime.max) }),
+  //       (x, y, z) => {
+  //         fc.pre(x < y)
+
+  //         const start = DateTime.fromDate(x)
+  //         const end = DateTime.fromDate(y)
+  //         const toCheck = DateTime.fromDate(z)
+
+  //         DateTime.assertIsDateTime(start)
+  //         DateTime.assertIsDateTime(end)
+  //         DateTime.assertIsDateTime(toCheck)
+
+  //         const assertion = assertThat(DateTime.isBetween({ start, end })(toCheck))
+
+  //         if (x < z) assertion.withMessage(`${toCheck.value} is not between ${start.value} - ${end.value}`).is(true)
+  //         else assertion.withMessage(`${toCheck.value} is between ${start.value} - ${end.value}`).is(false)
+  //       }
+  //     )
+  //   )
+  // })
+})
---
+++ src/index.ts
@@ -0,0 +1,97 @@
+import * as tzfns from 'date-fns-tz'
+import * as datefns from 'date-fns'
+import enNZ from 'date-fns/locale/en-NZ'
+
+declare const valieDateTime: unique symbol
+
+export type DateTimeStr = string & { [valieDateTime]: true }
+
+const format = `yyyy-MM-dd'T'HH:mm:ss.SSSxxx`
+
+type Timezone = 'UTC' | 'Pacific/Auckland'
+
+export type InvalidDate = {
+  _tag: 'InvalidDate'
+  message: string
+}
+
+const invalidDate = (date: string): InvalidDate => ({
+  _tag: 'InvalidDate',
+  message: `Provided JS Date ${date} is invalid`
+})
+
+export type DateTime = {
+  _tag: 'DateTime'
+  value: DateTimeStr
+  tz: Timezone
+}
+
+const defaultOptions: tzfns.OptionsWithTZ = {
+  locale: enNZ,
+  timeZone: 'UTC'
+}
+
+const of = (dateStr: string, tz: Timezone = 'UTC'): DateTime => ({
+  _tag: 'DateTime',
+  value: dateStr as DateTimeStr,
+  tz
+})
+
+export namespace DateTime {
+  export function isDateTime(x: any): x is DateTime {
+    return x._tag && x._tag === 'DateTime'
+  }
+
+  export function assertIsDateTime(x: any): asserts x is DateTime {
+    if (isDateTime(x)) return
+    throw new Error(`${JSON.stringify(x)} is not a DateTime object`)
+  }
+
+  const fromDateUnsafe = (date: Date): DateTime => {
+    const utcDate = tzfns.formatInTimeZone(date, 'UTC', format, defaultOptions)
+
+    return of(utcDate)
+  }
+
+  export const fromDate = (date: Date): InvalidDate | DateTime => {
+    if (!datefns.isValid(date)) return invalidDate(date.toJSON())
+
+    return fromDateUnsafe(date)
+  }
+
+  export const now = () => fromDate(new Date())
+
+  export const min = fromDateUnsafe(new Date(-1000000000000))
+
+  export const max = fromDateUnsafe(new Date(10000000000000))
+
+  export const fromEpoch = (epoch: number): InvalidDate | DateTime => {
+    const date = new Date(epoch)
+    if (!datefns.isValid(date)) return invalidDate(date.toJSON())
+
+    const utcDate = tzfns.formatInTimeZone(date, 'UTC', format, defaultOptions)
+    return of(utcDate)
+  }
+
+  export const toDate = (dateTime: DateTime): Date => tzfns.toDate(dateTime.value, defaultOptions)
+
+  export const isBefore = (x: DateTime) => (y: DateTime) => datefns.isBefore(toDate(y), toDate(x))
+  export const isAfter = (x: DateTime) => (y: DateTime) => isBefore(y)(x)
+
+  // export const isBetween = (cfg: { start: DateTime; end: DateTime }) => (toCheck: DateTime) =>
+  //   datefns.isWithinInterval(toDate(toCheck), { start: toDate(cfg.start), end: toDate(cfg.end) })
+
+  export const makeMicroseconds = (utcDate: DateTimeStr): number => {
+    const parts = utcDate!.split('.')
+    console.log(parts)
+    return parts.length > 1 ? toMicro(parts[1].split(/[\+\-]/)[0]) : 0
+  }
+
+  const toMicro = (s: string): number => {
+    return s.length > 0 ? padToNumber(s.replace(/[zZ]/, '').substring(0, 6)) : 0
+  }
+
+  const padToNumber = (aNumber: string): number => {
+    return Number('0.' + aNumber)
+  }
+}
